cf2e7237793b8dada2a5fc20e7cdab0c5f8759f5
suraj <surajmishra9318@gmail.com>
2023-03-06 22:39:35 +0530

    added theme line page

---
+++ src/componets/ThemeLine.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ThemeLine = () => {
+  return (
+    <div>
+      <div className='container-fluid theme'>
+        <p className='para1'>Don't let the Pressure of</p>
+            <h1>DEADLINES</h1>
+            <p className='para2'>Discourages you.</p>
+        <p className='para3'>Use it as motivation to work smarter and more efficiently. Take a deep breath, stay focused, and keep moving forward.</p>
+      </div>
+    </div>
+  )
+}
+
+export default ThemeLine
