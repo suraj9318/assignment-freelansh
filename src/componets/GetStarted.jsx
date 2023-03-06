73c987be07a0f51ff3f45eebbeb54eacaabcdb4a
suraj <surajmishra9318@gmail.com>
2023-03-06 17:21:26 +0530

    get started done

--- src/componets/GetStarted.jsx
+++ src/componets/GetStarted.jsx
@@ -5,7 +5,9 @@ const GetStarted = () => {
     <div className='container-fluid flex justify-center'>
       <div className='oval'>
         <h2>One-Stop-Shop</h2>
-        <p>for high-quality digital design assets <br/> that will take your creative projects to <br/> the next level.</p>
+        <p>for high-quality digital design assets <br/> that will take your creative projects to <br/> the <s>next</s> level.</p>
+        <span>Your</span>
+        <button className='get-started'>Get Started</button>
       </div>
     </div>
   )
