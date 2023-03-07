f30cf407ec5cd9635ab8967e973b0d19cda3c96c
suraj <surajmishra9318@gmail.com>
2023-03-07 19:36:09 +0530

    responsive done for theme nav and get started

--- src/componets/ThemeLine.jsx
+++ src/componets/ThemeLine.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const ThemeLine = () => {
   return (
-    <div>
-      <div className='container theme'>
+    
+      <div className='container-fluid theme'>
         <p className='para1'>Don't let the Pressure of</p>
             <h1>DEADLINES</h1>
             <p className='para2'>Discourages you.</p>
         <p className='para3'>Use it as motivation to work smarter and more efficiently. Take a deep breath, stay focused, and keep moving forward.</p>
       </div>
-    </div>
+
   )
 }
 
