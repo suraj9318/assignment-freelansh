df1d8c2cd60f9549a3c0eb483e2a27937a8b8ea2
suraj <surajmishra9318@gmail.com>
2023-03-09 12:27:14 +0530

    design done for desktop application

--- src/componets/ThemeLine.jsx
+++ src/componets/ThemeLine.jsx
@@ -7,7 +7,16 @@ const ThemeLine = () => {
         <p className='para1'>Don't let the Pressure of</p>
             <h1>DEADLINES</h1>
             <p className='para2'>Discourages you.</p>
-        <p className='para3'>Use it as motivation to work smarter and more efficiently. Take a deep breath, stay focused, and keep moving forward.</p>
+
+
+        <div className='para-container'>
+            <p className='para3'>Use it as motivation to work smarter and more efficiently.</p>
+            <p className='para3'>Take a deep breath, stay focused, and keep moving</p>
+            <p className='para3'>forward .</p>
+        </div>
+
+
+
       </div>
 
   ) 
